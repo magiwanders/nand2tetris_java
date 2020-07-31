0aeb8b34d1ca08d6b38c23925d854a2e1c791cd2
magiwanders <magi.wanders@gmail.com>
2020-07-31 19:48:48 +0200

    fine impostazione

--- gui/Gui.java
+++ gui/Gui.java
@@ -1,7 +1,104 @@
 package gui;
 
-public class Gui {
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.event.* ;
+
+public class Gui extends Stage {
+
+  private VBox root;
+
+    private HBox jackFile;
+      private Label label1;
+      private TextField textField1;
+      private Button load1;
+
+    private Button compileButton;
+
+    private HBox vmcode;
+      private Label label2;
+      private TextField textField2;
+      private Button load2;
+
+    private Button translateButton;
+
+    private HBox assembly;
+      private Label label3;
+      private TextField textField3;
+      private Button load3;
+
+    private Button assembleButton;
+
+    private HBox binary;
+      private Label label4;
+      private TextField textField4;
+
+    private Label errorLabel;
+
+    public Gui() {
+        super();
+
+        root = new VBox();
+
+        jackFile = new HBox();
+          label1 = new Label("Jack File: ");
+          textField1 = new TextField();
+          load1 = new Button("Load");
+
+        compileButton = new Button("Compile");
+        vmcode = new HBox();
+          label2 = new Label("VMCode: ");
+          textField2 = new TextField();
+          load2 = new Button("Load");
 
 
+        translateButton = new Button("Translate");
+        assembly = new HBox();
+          label3 = new Label("Assembly File: ");
+          textField3 = new TextField();
+          load3 = new Button("Load");
+
+        assembleButton = new Button("Assemble");
+        binary = new HBox();
+          label4 = new Label("Binary File: ");
+          textField4 = new TextField();
+
+        errorLabel = new Label();
+
+        assembleGUI();
+
+        super.setTitle("nand2tetris");
+        super.setScene(new Scene(root));
+        super.setWidth(500);
+        super.setHeight(300);
+        super.show();
+    }
+
+    private void assembleGUI() {
+      root.getChildren().add(jackFile);
+        jackFile.getChildren().add(label1);
+        jackFile.getChildren().add(textField1);
+        jackFile.getChildren().add(load1);
+      root.getChildren().add(compileButton);
+      root.getChildren().add(vmcode);
+        vmcode.getChildren().add(label2);
+        vmcode.getChildren().add(textField2);
+        vmcode.getChildren().add(load2);
+      root.getChildren().add(translateButton);
+      root.getChildren().add(assembly);
+        assembly.getChildren().add(label3);
+        assembly.getChildren().add(textField3);
+        assembly.getChildren().add(load3);
+      root.getChildren().add(assembleButton);
+      root.getChildren().add(binary);
+        binary.getChildren().add(label4);
+        binary.getChildren().add(textField4);
+      root.getChildren().add(errorLabel);
+      errorLabel.setWrapText(true);
+
+    }
 
 }
--- nand2tetris.java
+++ nand2tetris.java
@@ -1,3 +1,48 @@
-import gui;
-import assembler;
-import VMTranslator;
+// From nand2tetris_java folder
+// export PATH_TO_FX=secondaprovainitinere/src/javafx/lib
+// javac --module-path $PATH_TO_FX --add-modules javafx.controls nand2tetris.java
+// java --module-path $PATH_TO_FX --add-modules javafx.controls secondaprovainitinere.src.Client
+
+import gui.Gui;
+//import assembler;
+// import VMTranslator;
+
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.event.* ;
+
+import java.io.*;
+
+public class nand2tetris extends Application {
+
+  private Gui gui;
+
+  private void setActions() {
+    //gui.getAssemblerButton().setOnAction( this::assembleButtonHandle() );
+  }
+
+  @Override
+  public void init() throws Exception {
+    System.out.println("Nand2Tetris program initializing...");
+  }
+
+  @Override
+  public void start(Stage window) throws Exception {
+    gui = new Gui();
+    setActions();
+  }
+
+  @Override
+  public void stop() throws Exception {
+    System.out.println("Exiting.");
+
+  }
+
+  public static void main(String [] args) {
+    launch();
+  }
+
+}
