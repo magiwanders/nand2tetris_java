57c68d9cec67ca29bf06f9872cff97ff8f237dd4
magiwanders <magi.wanders@gmail.com>
2020-09-13 23:07:53 +0200

    Beginning of CPU test

---
+++ hackComputer/tests/CPUTest.java
@@ -0,0 +1,19 @@
+package hackComputer.tests;
+
+import hackComputer.CPU.CPU;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CPUTest {
+    @Test
+    public void test() {
+        CPU cpu = new CPU();
+        boolean [] instruction, inM;
+
+        instruction = program[i];
+        inM = inMs[i];
+        reset = resets[i];
+        cpu.update
+
+    }
+}
