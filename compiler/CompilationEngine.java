de5dfd0948b6dac29ca8f3679eafeba6d06b5265
magiwanders <magi.wanders@gmail.com>
2020-09-13 13:51:02 +0200

    CompilationEngine working up to 'average' test

--- compiler/CompilationEngine.java
+++ compiler/CompilationEngine.java
@@ -157,9 +157,29 @@ public class CompilationEngine {
         index++; // "let"
         String name = peekNext(); index++; // varName
         if(peekNext().equals("[")) {
+            if (subroutineTable.contains(name)) {
+                String kind = subroutineTable.kindOf(name);
+                int i = subroutineTable.indexOf(name);
+                write("push " + kind + " " + i);
+            } else {
+                if (classTable.contains(name)) {
+                    String kind = classTable.kindOf(name);
+                    int i = classTable.indexOf(name);
+                    write("push " + kind + " " + i);
+                }
+            }
             index++; // [
             compileExpression();
             index++; // ]
+            write("add");
+            index++; // =
+            compileExpression();
+            index++; // ;
+            write("pop temp 0");
+            write("pop pointer 1");
+            write("push temp 0");
+            write("pop that 0");
+            return;
         }
         index++; // =
         compileExpression();
@@ -297,25 +317,43 @@ public class CompilationEngine {
             int currentInt = Integer.parseInt(peekNext());
             write("push constant " + currentInt);
             index++; // Integer
-        }
-        else if (peekNextType().equals("stringConstant")) index++; // String
-        else if (isKeywordConstant()) {
+        } else if (peekNextType().equals("stringConstant")) {
+            String string = peekNext(); index++; // String
+            write("push constant " + string.length());
+            write("call String.new 1");
+            for(char c : string.toCharArray()) {
+                write("push constant " + charToNumber(c));
+                write("call String.appendChar 2");
+            }
+        } else if (isKeywordConstant()) {
             write(keywordConstantValue()); index++; // KeyboardConstant
-        }
-        else if (peekNext().length()==1 && (peekNext().charAt(0)=='~' || peekNext().charAt(0)=='-')) {
+        } else if (peekNext().length()==1 && (peekNext().charAt(0)=='~' || peekNext().charAt(0)=='-')) {
             String op = peekNext(); index++; // UnaryOp
             compileTerm();
             write(unaryOpToVM(op));
-        }
-        else if (peekNext().length()==1 && peekNext().charAt(0)=='(') {
+        } else if (peekNext().length()==1 && peekNext().charAt(0)=='(') {
             index++; // (
             compileExpression();
             index++; // )
         } else if (isLetter(peekNext().charAt(0)) && peekFurther().equals("[")) {
-            index++; // Var name
+            String name = peekNext(); index++; // Var name
+            if (subroutineTable.contains(name)) {
+                String kind = subroutineTable.kindOf(name);
+                int i = subroutineTable.indexOf(name);
+                write("push " + kind + " " + i);
+            } else {
+                if (classTable.contains(name)) {
+                    String kind = classTable.kindOf(name);
+                    int i = classTable.indexOf(name);
+                    write("push " + kind + " " + i);
+                }
+            }
             index++; // [
             compileExpression();
             index++; // ]
+            write("add");
+            write("pop pointer 1");
+            write("push that 0");
         } else if (isLetter(peekNext().charAt(0)) && (peekFurther().equals("(") || peekFurther().equals("."))) {
             compileSubroutineCall();
         } else if (!(isLetter(peekNext().charAt(0)) && (peekFurther().equals("(") || peekFurther().equals(".") || peekFurther().equals("[")))) {
@@ -366,6 +404,59 @@ public class CompilationEngine {
             case "-": return "neg";
         }
         return "";
+    }
+
+    private int charToNumber(char c) {
+        if(c=='a') return 97; if(c=='A') return 65;
+        if(c=='b') return 98; if(c=='B') return 66;
+        if(c=='c') return 99; if(c=='C') return 67;
+        if(c=='d') return 100; if(c=='D') return 68;
+        if(c=='e') return 101; if(c=='E') return 69;
+        if(c=='f') return 102; if(c=='F') return 70;
+        if(c=='g') return 103; if(c=='G') return 71;
+        if(c=='h') return 104; if(c=='H') return 72;
+        if(c=='i') return 105; if(c=='I') return 73;
+        if(c=='j') return 106; if(c=='J') return 74;
+        if(c=='k') return 107; if(c=='K') return 75;
+        if(c=='l') return 108; if(c=='L') return 76;
+        if(c=='m') return 109; if(c=='M') return 77;
+        if(c=='n') return 110; if(c=='N') return 78;
+        if(c=='o') return 111; if(c=='O') return 79;
+        if(c=='p') return 112; if(c=='P') return 80;
+        if(c=='q') return 113; if(c=='Q') return 81;
+        if(c=='r') return 114; if(c=='R') return 82;
+        if(c=='s') return 115; if(c=='S') return 83;
+        if(c=='t') return 116; if(c=='T') return 84;
+        if(c=='u') return 117; if(c=='U') return 85;
+        if(c=='v') return 118; if(c=='V') return 86;
+        if(c=='w') return 119; if(c=='W') return 87;
+        if(c=='x') return 120; if(c=='X') return 88;
+        if(c=='y') return 121; if(c=='Y') return 89;
+        if(c=='z') return 122; if(c=='Z') return 90;
+
+        if(c=='0') return 48; if(c==':') return 58;
+        if(c=='1') return 49; if(c==';') return 59;
+        if(c=='2') return 50; if(c=='<') return 60;
+        if(c=='3') return 51; if(c=='=') return 61;
+        if(c=='4') return 52; if(c=='>') return 62;
+        if(c=='5') return 53; if(c=='?') return 63;
+        if(c=='6') return 54; if(c=='@') return 64;
+        if(c=='7') return 55; if(c=='[') return 91;
+        if(c=='8') return 56; if(c=='\\') return 92;
+        if(c=='9') return 57; if(c==']') return 93;
+
+        if(c=='^') return 94; if(c=='_') return 95;
+        if(c==' ') return 32; if(c=='"') return 90;
+        if(c=='#') return 34; if(c=='$') return 36;
+        if(c=='%') return 37; if(c=='&') return 38;
+        if(c=='\'') return 39; if(c=='(') return 40;
+        if(c==')') return 41; if(c=='*') return 42;
+        if(c=='+') return 43; if(c==',') return 44;
+        if(c=='.') return 46; if(c=='/') return 47;
+        if(c=='!') return 35;
+
+        if(c=='\n') return 128;
+        return -1;
     }
 
     private boolean isLetter(char candidate) {
@@ -441,8 +532,8 @@ public class CompilationEngine {
         int indexLimit = simpleLine.indexOf(">", indexOfClosing);
         complexLine[0] = simpleLine.substring(indexOfClosing+2, indexLimit);
         int indexOfFirstSpace = simpleLine.indexOf(" ");
-        int indexOfSecondSpace = simpleLine.indexOf(" ", indexOfFirstSpace+1);
-        complexLine[1] = simpleLine.substring(indexOfFirstSpace+1, indexOfSecondSpace);
+        // int indexOfSecondSpace = simpleLine.indexOf(" ", indexOfFirstSpace+1);
+        complexLine[1] = simpleLine.substring(indexOfFirstSpace+1, indexOfClosing-1);
         index--;
         return complexLine;
     }
